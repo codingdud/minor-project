a529b7307364669d325a2fe3810a6db3ebc33e19
codingdud <codingdud@gmail.com>
2024-03-09 04:42:33 +0530

    dockerfile

--- backend/app.js
+++ backend/app.js
@@ -9,12 +9,8 @@ const app=express()
 app.use(express.json())
 app.use(cors())
 
-
-app.use('/health',(req,res)=>res.json("i am ok.."))
+//app.use('/',(req,res)=>res.json({data:"i am ok.."}))
 app.use('/api',require('./routes/placeRouter'))
-
-/* app.use('/api/fdata',require('./routes/fdata-route'));
-app.use('/api/sdata',require('./routes/sdata-route')); */
 
 //conneting to database and listing to port
 const mongoid=process.env.MID
@@ -22,7 +18,7 @@ let db=process.env.DB||"database"
 //console.log(mongoid,db)
 mongoose.connect(`${mongoid}${db}?retryWrites=true&w=majority`)
     .then(()=>{
-        const port=process.env.PORT||3001
+        const port=process.env.PORT||8080
             app.listen(port,()=>{
             console.log(`connect to port:${port}
             database is active`)
--- backend/controllers/placeController.js
+++ backend/controllers/placeController.js
@@ -1,27 +1,28 @@
 const mongoose=require('mongoose')
+
 const getPlace=async(req,res)=>{
-    const collections = await mongoose.connection.db.collection('placedatas').find().toArray();
+    const collections = await mongoose.connection.collection('placedatas').find().toArray();
     return res.json(collections)
 }
 const getNearByPlace=async(req,res)=>{
-    let {latitude,longitude,distance}=req.body;
-    //console.log(latitude,longitude,distance)
-    if(!latitude || !longitude){
-        return res.status(401).send({
-            message: "Missing fields"
-          });
-    }else{
-        const nearbylocation = await mongoose.connection.db.collection('placedatas').find({
-            loc:
-              { $near:
-                 {
-                   $geometry: { type: "Point",  coordinates: [latitude,longitude] },
-                   $maxDistance:distance
-                 }
-              }
-          }).toArray();
-        return res.json(nearbylocation)
-    } 
+  let {latitude,longitude,distance}=req.body;
+  //console.log(latitude,longitude,distance)
+  if(!latitude || !longitude){
+      return res.status(401).send({
+          message: "Missing fields"
+        });
+  }else{
+      const nearbylocation = await mongoose.connection.collection('placedatas').find({
+          loc:
+            { $near:
+                {
+                  $geometry: { type: "Point",  coordinates: [latitude,longitude] },
+                  $maxDistance:distance
+                }
+            }
+        }).toArray();
+      return res.json(nearbylocation)
+  } 
 }
 
 const getNearByFoods=async(req,res)=>{
@@ -32,7 +33,7 @@ const getNearByFoods=async(req,res)=>{
           message: "Missing fields"
         });
   }else{
-      const nearbylocation = await mongoose.connection.db.collection('foods').find({
+      const nearbylocation = await mongoose.connection.collection('foods').find({
           loc:
             { $near:
                {
@@ -53,7 +54,7 @@ const getNearByColleges=async(req,res)=>{
           message: "Missing fields"
         });
   }else{
-      const nearbylocation = await mongoose.connection.db.collection('colleges').find({
+      const nearbylocation = await mongoose.connection.collection('colleges').find({
           loc:
             { $near:
                {
@@ -74,7 +75,7 @@ const getNearByUnivercities=async(req,res)=>{
           message: "Missing fields"
         });
   }else{
-      const nearbylocation = await mongoose.connection.db.collection('universities').find({
+      const nearbylocation = await mongoose.connection.collection('universities').find({
           loc:
             { $near:
                {
